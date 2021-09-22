8dd2a8ff18429f366e67b4fc72ab98c38d560ecb
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-22 14:14:38 +0200

    creating CartItem

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -1,11 +1,26 @@
 import Button from "@material-ui/core/Button";
 //Types
 import { CartItemType } from "../App";
+import Item from "../Item/Item";
 //Styles
 import { Wrapper } from "./CartItem.styles";
 
-const CartItem: React.FC = () => {
-  return <div>Cart Item</div>;
+type Props = {
+  item: CartItemType;
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
 };
+
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+  <Wrapper>
+    <div>
+      <h3>{item.title}</h3>
+      <div className="information">
+        <p>Price: ${item.price}</p>
+        <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+      </div>
+    </div>
+  </Wrapper>
+);
 
 export default CartItem;
