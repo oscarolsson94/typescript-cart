ec5f9f0fb7f77fffb76fedac2b799f286f2ef4a4
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-22 09:47:47 +0200

    setting up function to get data from api + type

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,20 @@ import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
 //Styles
 import { Wrapper } from "./App.styles";
+//Types
+//response from api
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
   return <div className="App">Start</div>;
