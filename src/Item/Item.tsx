7dd2b7674d98fd361e1292c61713b6e44a047ddd
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-22 11:40:54 +0200

    creating Item component

---
+++ src/Item/Item.tsx
@@ -0,0 +1,16 @@
+import Button from "@material-ui/core/Button";
+//Types
+import { CartItemType } from "../App";
+//Styles
+import { Wrapper } from "./Item.styles";
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart: 
+}
+
+const Item = () => {
+  return <div></div>;
+};
+
+export default Item;
